04f71f3e08e7e7822d5f02dc1df247583949674f
Stephen Ochieng <materiamystic@gmail.com>
2024-06-08 23:50:04 +0300

    Adds methods to map data from python class to a relational db

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,36 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute('CREATE TABLE IF NOT EXISTS departments (id INTEGER PRIMARY KEY, name TEXT, location TEXT);')
+        CONN.commit()
+    
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute('DROP TABLE IF EXISTS departments;')
+        CONN.commit()
+        
+    def save(self):
+        CURSOR.execute('INSERT INTO departments (name, location) VALUES (?, ?)', (self.name, self.location))
+        CONN.commit()
+        
+        self.id = CURSOR.lastrowid
+        
+    @classmethod
+    def create(cls, name, location):
+        """ Initialize a new Department instance and save the object to the database """
+        department = cls(name, location)
+        department.save()
+        return department
+    
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        CURSOR.execute('UPDATE departments SET name = ?, location = ? WHERE id = ?', (self.name, self.location, self.id))
+        CONN.commit()
+        
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        CURSOR.execute('DELETE FROM departments WHERE id = ?', (self.id,))
+        CONN.commit()
